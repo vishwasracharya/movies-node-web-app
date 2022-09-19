6b08c5fa6988dc8e8da8af6d57b19e6ec343274a
Vishwas Acharya <vishwasracharya@gmail.com>
2022-09-19 21:42:07 +0530

    db

---
+++ learn/mongodb/db.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+
+let db;
+let client;
+
+module.exports = {
+  db: () => db,
+  client: () => client,
+  connect: async () => {
+    client = MongoClient(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    db = client.db('moviesdb');
+  },
+  close: async () => client.close(),
+};
